7f6c6832a79560b58182b403da4db3670fccb994
Eugene Menski <eugene.menski@gmail.com>
2023-04-30 13:37:49 +0400

    Added cmd_forecast_points_days method

--- analysis.py
+++ analysis.py
@@ -1223,6 +1223,22 @@ def cmd_forecast_points_n(output, issue_data, since='', until='', n=10, simulati
                           forecast_summary)
 
 
+def cmd_forecast_points_days(output, issue_data, since='', until='', days=10, simulations=10000, window=90):
+    # Process forecast points days command
+    # pre-req
+    t, tw = process_throughput_data(issue_data, since=since, until=until)
+    # analysis
+    mh, s = forecast_montecarlo_how_many_points(t, days=days, simulations=simulations, window=window)
+
+    forecast_summary = pandas.DataFrame.from_records([
+        (f'{int(q*100)}%', s.Points.quantile(1-q)) for q in (0.25, 0.5, 0.75, 0.85, 0.95, 0.999)
+    ], columns=('Probability', 'points'), index='Probability')
+
+    output_formatted_data(output,
+                          f'Montecarlo Forecast: How many points can be completed within {days} days?',
+                          forecast_summary)
+
+
 def run(args):
     data, dupes, filtered = read_data(args.file,
                                       exclude_types=args.exclude_type,
