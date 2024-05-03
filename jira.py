1f3815af69a0f1083738b1c1b76e25f54c7ef364
Eugene Menski <eugene.menski@gmail.com>
2024-05-03 17:21:30 +0400

    Refactoring

--- jira.py
+++ jira.py
@@ -396,47 +396,59 @@ def generate_output_csv(client,
     logging.info(f'{count} records written')
 
 
-def main():
-    domain = config('JIRA_DOMAIN')
-    email = config('JIRA_EMAIL')
-    apikey = config('JIRA_APIKEY')
-    output_file = config('JIRA_OUTPUT_FILE')
-
+def make_parser(domain, email, apikey, output_file):
     parser = argparse.ArgumentParser(description='Extract changelog of Jira project issue')
-
-    parser.add_argument('project', help='Jira project from which to extract issues')
-    parser.add_argument('since', help='Date from which to start extracting issues (yyyy-mm-dd)')
-
+    parser.add_argument('project',
+                        help='Jira project from which to extract issues')
+    parser.add_argument('since',
+                        help='Date from which to start extracting issues (yyyy-mm-dd)')
     parser.add_argument('--updates-only',
                         action='store_true',
-                        help="When passed, instead of extracting issues created since the since argument, only issues "
-                             "*updated* since the since argument will be extracted.")
+                        help="When passed, instead of extracting issues created since the since argument, "
+                             "only issues *updated* since the since argument will be extracted.")
     parser.add_argument('--append',
                         action='store_true',
                         help='Append to the output file instead of overwriting it.')
     parser.add_argument('--anonymize',
                         action='store_true',
                         help='Anonymize the data output (no issue titles, project keys, etc).')
-
-    parser.add_argument('-d', '--domain', default=domain,
-                        help='Jira project domain url (i.e., https://company.atlassian.net). Can also be provided via '
-                             'JIRA_DOMAIN environment variable.')
-    parser.add_argument('-e', '--email', default=email,
-                        help='Jira user email address for authentication. Can also be provided via JIRA_EMAIL '
-                             'environment variable.')
-    parser.add_argument('-k', '--apikey', default=apikey,
-                        help='Jira user api key for authentication. Can also be provided via JIRA_APIKEY environment '
-                             'variable.')
-
-    parser.add_argument('-o', '--output', default=output_file, help='File to store the csv output.')
-
-    parser.add_argument('-q', '--quiet', action='store_true', help='Be quiet and only output warnings to console.')
-
-    parser.add_argument('-f', '--field', metavar='FIELD_ID', action='append',
+    parser.add_argument('-d', '--domain',
+                        default=domain,
+                        help='Jira project domain url (i.e., https://company.atlassian.net). '
+                             'Can also be provided via JIRA_DOMAIN environment variable.')
+    parser.add_argument('-e', '--email',
+                        default=email,
+                        help='Jira user email address for authentication. '
+                             'Can also be provided via JIRA_EMAIL environment variable.')
+    parser.add_argument('-k', '--apikey',
+                        default=apikey,
+                        help='Jira user api key for authentication. '
+                             'Can also be provided via JIRA_APIKEY environment variable.')
+    parser.add_argument('-o', '--output',
+                        default=output_file,
+                        help='File to store the csv output.')
+    parser.add_argument('-q', '--quiet',
+                        action='store_true',
+                        help='Be quiet and only output warnings to console.')
+    parser.add_argument('-f', '--field',
+                        metavar='FIELD_ID',
+                        action='append',
                         help='Include one or more custom fields in the query by id.')
-    parser.add_argument('-n', '--name', metavar='FIELD_NAME', action='append',
+    parser.add_argument('-n', '--name',
+                        metavar='FIELD_NAME',
+                        action='append',
                         help='Corresponding output column names for each custom field.')
+    return parser
 
+
+def main():
+
+    domain = config('JIRA_DOMAIN')
+    email = config('JIRA_EMAIL')
+    apikey = config('JIRA_APIKEY')
+    output_file = config('JIRA_OUTPUT_FILE')
+
+    parser = make_parser(domain=domain, email=email, apikey=apikey, output_file=output_file)
     args = parser.parse_args()
 
     if not args.quiet:
