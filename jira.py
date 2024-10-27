e5ce38fce77578bb5cdf7dab86be347c64b524cd
Eugene Menski <eugene.menski@gmail.com>
2024-10-27 20:54:07 +0400

    Refactor logging

--- jira.py
+++ jira.py
@@ -1,6 +1,5 @@
 import requests_cache
 import sys
-import logging
 
 from jira.utils.config import Config
 from jira.utils.arg_parser import JiraArgumentParser
@@ -12,9 +11,7 @@ from jira.utils.exceptions import (JiraConfigurationError,
                                    JiraReportGenerationError,
                                    JiraArgumentError)
 
-
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
+from jira.utils.logger_config import LoggerConfig
 
 
 def setup_cache():
@@ -22,17 +19,13 @@ def setup_cache():
     requests_cache.install_cache(cache_name='jira_cache', backend='sqlite', expire_after=24 * 60 * 60)
 
 
-def set_logging_level(quiet_mode: bool) -> None:
-    """Set the logging level based on the quiet mode flag."""
-    log_level = logging.WARNING if quiet_mode else logging.INFO
-    logging.getLogger().setLevel(log_level)
-    logger.setLevel(log_level)
-
-
 def main():
     """Main function to execute the JIRA data extraction and report generation."""
 
     setup_cache()
+
+    logger_config = LoggerConfig()
+    logger = logger_config.get_logger(__name__)
 
     try:
         config_values = Config().values()
@@ -42,19 +35,17 @@ def main():
 
     try:
         args = JiraArgumentParser(**config_values).parse()
+        logger_config.setup_quiet_logging(args.quiet)
     except JiraArgumentError as e:
         logger.error(f"Argument parsing error: {e}")
         sys.exit(1)
-
-    set_logging_level(args.quiet)
 
     try:
         report_generator = JiraReportGenerator(args)
         report_generator.run()
     except (JiraConnectionError,
             JiraDataExtractionError,
-            JiraReportGenerationError,
-            ValueError) as e:
+            JiraReportGenerationErrorValueError) as e:
         logger.error(f"Report generation failed: {e}")
         sys.exit(1)
 
